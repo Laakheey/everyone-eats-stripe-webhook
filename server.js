fca85f3c48de6d77d9273bd2fbb50ac33f3d93cf
Laakheey <sunilpandey0086@gmail.com>
2024-06-28 19:31:51 +0545

    log added

--- server.js
+++ server.js
@@ -13,12 +13,12 @@
 
 // The library needs to be configured with your account's secret key.
 // Ensure the key is kept out of any version control system you might be using.
-const stripe = require('stripe')(process.env.STRIPE_PUBLISHABLE_KEY);
-const express = require('express');
+const stripe = require("stripe")(process.env.STRIPE_PUBLISHABLE_KEY);
+const express = require("express");
 const app = express();
-const Payments = require('./paymentDB');
-const mongoose = require('mongoose');
-require('dotenv').configDotenv();
+const Payments = require("./paymentDB");
+const mongoose = require("mongoose");
+require("dotenv").configDotenv();
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -27,43 +27,51 @@ mongoose
 
 const endpointSecret = process.env.END_POINT_SECRET;
 
-const PORT = process.env.PORT || 4242
+const PORT = process.env.PORT || 4242;
 
-app.get('/', (req, res) => {
-    return res.json({msg: 'hello world'});
+app.get("/", (req, res) => {
+  return res.json({ msg: "hello world" });
 });
 
-app.post('/webhook', express.raw({type: 'application/json'}), async (request, response) => {
-  const sig = request.headers['stripe-signature'];
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (request, response) => {
+    console.log("Weebhook api called");
+    const sig = request.headers["stripe-signature"];
 
-  let event;
+    let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-  } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
-    return;
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      console.log(
+        `Webhook signature verification failed. Error: ${err.message}`
+      );
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    let sessionData;
+    switch (event.type) {
+      case "checkout.session.completed":
+        const checkoutSessionCompleted = event.data.object;
+        console.log("checkoutSessionCompleted", checkoutSessionCompleted);
+        sessionData = checkoutSessionCompleted;
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    const data = new Payments({
+      userId: new mongoose.Types.ObjectId("666ee91689a6f624d7f80cfd"),
+      restaurantID: new mongoose.Types.ObjectId("66573f6211cd76caa8c567ef"),
+      stripeId: sessionData.id,
+    });
+    await data.save();
+
+    response.send();
   }
+);
 
-  let sessionData;
-  switch (event.type) {
-    case 'checkout.session.completed':
-      const checkoutSessionCompleted = event.data.object;
-      console.log("checkoutSessionCompleted", checkoutSessionCompleted);
-      sessionData = checkoutSessionCompleted;
-      break;
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  const data = new Payments({
-    userId: new mongoose.Types.ObjectId('666ee91689a6f624d7f80cfd'),
-    restaurantID: new mongoose.Types.ObjectId('66573f6211cd76caa8c567ef'),
-    stripeId: sessionData.id
-  });
-  await data.save();
-
-  response.send();
-});
-
-app.listen(PORT, () => console.log('http://localhost:4242'));
+app.listen(PORT, () => console.log("http://localhost:4242"));
